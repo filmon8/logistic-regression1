5b890efe63ef86c179ed4913a42842139c70544e
filmon8 <filmonkfl@gmail.com>
2020-06-03 00:06:33 -0500

    predict.m is updated with vectorized solution in neural network using feed forward.

--- predict.m
+++ predict.m
@@ -21,9 +21,14 @@ p = zeros(size(X, 1), 1);
 %       can use max(A, [], 2) to obtain the max for each row.
 %
 
+X=[ones(m,1) X];
 
+a2=sigmoid(X*Theta1');
+t2=[ones(m,1) a2];
 
+a3=sigmoid(t2*Theta2');
 
+[~,p]=max(a3,[],2);
 
 
 
